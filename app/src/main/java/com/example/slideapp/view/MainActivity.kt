4e64b2f2de6d428c3e083f69e725dedd2a919b5e
HyeonSeongKang <gt9697@naver.com>
2023-07-17 16:40:43 +0900

    feat: 화면 터치

--- app/src/main/java/com/example/slideapp/view/MainActivity.kt
+++ app/src/main/java/com/example/slideapp/view/MainActivity.kt
@@ -1,15 +1,43 @@
 package com.example.slideapp.view
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
+import android.view.View
 import com.example.slideapp.R
+import com.example.slideapp.databinding.ActivityMainBinding
 import com.example.slideapp.model.SlideSquareView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.rootView.setOnTouchListener { _, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    if (isViewTouched(binding.squareView, event.x, event.y)) {
+                        binding.squareView.setBackgroundResource(R.drawable.yellow_square)
+                        Log.d("로그", "Square is touched")
+                    } else {
+                        binding.squareView.setBackgroundColor(Color.YELLOW)
+                        Log.d("로그", "Background is touched")
+                    }
+                    true
+                }
+                else -> false
+            }
+        }
 
         val react1 = SlideSquareView.createRandomSlideSquareView()
         Log.d("로그", react1.toString())
@@ -23,5 +51,15 @@ class MainActivity : AppCompatActivity() {
         val react4 = SlideSquareView.createRandomSlideSquareView()
         Log.d("로그", react4.toString())
 
+    }
+
+    private fun isViewTouched(view: View, touchX: Float, touchY: Float): Boolean {
+        val location = IntArray(2)
+        view.getLocationOnScreen(location)
+        val left = location[0]
+        val top = location[1]
+        val right = left + view.width
+        val bottom = top + view.height
+        return touchX >= left && touchX <= right && touchY >= top && touchY <= bottom
     }
 }
